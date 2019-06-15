87ea38b6eb12a048b583bef2d4c1ad4215b98cbc
Arvind Kumar Singh <samratarvind114@gmail.com>
2019-06-15 14:34:23 +0530

    changed admin.py in accounts app

--- accounts/admin.py
+++ accounts/admin.py
@@ -4,7 +4,6 @@ from django.contrib import admin
 from django.contrib.auth.models import Group
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-
 from .forms import UserAdminCreationForm, UserAdminChangeForm
 from .models import User
 
@@ -12,7 +11,6 @@ class UserAdmin(BaseUserAdmin):
     # The forms to add and change user instances
     form = UserAdminChangeForm
     add_form = UserAdminCreationForm
-
     # The fields to be used in displaying the User model.
     # These override the definitions on the base UserAdmin
     # that reference specific fields on auth.User.
@@ -34,7 +32,6 @@ class UserAdmin(BaseUserAdmin):
     search_fields = ('email',)
     ordering = ('email',)
     filter_horizontal = ()
-
 
 admin.site.register(User, UserAdmin)
 
