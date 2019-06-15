7a76fece5dc35d0330196493cb6f0e73b88eb849
Arvind Kumar Singh <samratarvind114@gmail.com>
2019-06-15 14:31:50 +0530

    changed admin.py in billing app

--- billing/admin.py
+++ billing/admin.py
@@ -7,11 +7,6 @@ class BillingProfileAdmin(admin.ModelAdmin):
 	list_display = ['email']
 	class Meta:
 		model= BillingProfile
-	# def username(self,obj): #the obj is the object of 'BillingProfile', self is the object of 'BillingProfileAdmin'
-	# 	if obj.user.name:
-	# 		return obj.user.name
-	# 	return None
-
 admin.site.register(BillingProfile,BillingProfileAdmin)
 
 admin.site.register(Card)
